d2e1c8fe132fb5b29007d65cb387775075180508
amin <aminmirzaiee1374@gmail.com>
2022-03-09 15:41:56 +0330

    version controle configuration done

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import torch
+if __name__ == '__main__':
+    print("jjj")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
